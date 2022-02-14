e2b9b891e59d2bd5d46e1dc3eef414fd33226cae
PHILIP BARBER <billfarber@gmail.com>
2022-02-14 16:55:05 -0500

    Updated the example script file to reflect the changes to how joins are specified.

--- sampleGraphqlQuery.sh
+++ sampleGraphqlQuery.sh
@@ -10,31 +10,31 @@ echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
 printf "Result: $result\n\n\n"
 
-query="query humansCarsJoin { Humans { id name height Cars { ownerId model year } id } }"
+query="query humansCarsJoin { Humans { id name height Cars { ownerId @childJoinColumn id @parentJoinColumn model year } } }"
 printf "Query: $query\n"
 echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
 printf "Result: $result\n\n\n"
 
-query="query humansCarsLaptopsHousesJoin  { Humans { id name height Cars { ownerId model year } id Laptops { ownerId model screenSize } id Houses { ownerId number street } id } }"
+query="query humansCarsLaptopsHousesJoin  { Humans { id name height Cars { ownerId @childJoinColumn id @parentJoinColumn model year } Laptops { ownerId @childJoinColumn id @parentJoinColumn model screenSize } Houses { ownerId @childJoinColumn id @parentJoinColumn number street } } }"
 printf "Query: $query\n"
 echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
 printf "Result: $result\n\n\n"
 
-query="query humansArgumentCarsJoin { Humans (id: "1000") { id name height Cars { ownerId model year } id } }"
+query="query humansArgumentCarsJoin { Humans (id: "1000") { id name height Cars { ownerId @childJoinColumn id @parentJoinColumn model year } } }"
 printf "Query: $query\n"
 echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
 printf "Result: $result\n\n\n"
 
-query="query humansCarsArgumentJoin { Humans { id name height Cars(id: "2") { ownerId model year } id } }"
+query="query humansCarsArgumentJoin { Humans { id name height Cars(id: "2") { ownerId @childJoinColumn id @parentJoinColumn model year } } }"
 printf "Query: $query\n"
 echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
 printf "Result: $result\n\n\n"
 
-query="query nestedJoin { Humans { id name height Houses { ownerId number street Rooms { houseId type } id } id } }"
+query="query nestedJoin { Humans { id name height Houses { ownerId @childJoinColumn id @parentJoinColumn number street Rooms { houseId @childJoinColumn id @parentJoinColumn type } } } }"
 printf "Query: $query\n"
 echo $query > command.txt
 result=$(curl --digest --user admin:admin -s -X POST -H "Content-type: application/graphql" -d@./command.txt http://localhost:8004/LATEST/resources/graphql)
